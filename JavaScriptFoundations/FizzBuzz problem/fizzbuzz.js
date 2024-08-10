3c230279a9d45fe7a7765001aa47e3ba75969a0e
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2024-08-10 18:28:56 -0400

    added conditionals and an output

--- JavaScriptFoundations/FizzBuzz problem/fizzbuzz.js
+++ JavaScriptFoundations/FizzBuzz problem/fizzbuzz.js
@@ -1,1 +1,21 @@
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz upto: "));
+
+const list = [];
+
+for(let i = 1; i <= answer; i++) {
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+        list.push("FizzBuzz");
+    }
+    else if(i % 3 === 0){
+        console.log("Fizz");
+        list.push("Fizz");
+    }else if(i % 5 === 0){
+        console.log("Buzz");
+        list.push("Buzz");   
+    }else{
+        console.log(i);
+        list.push(i);
+    }
+}
+
