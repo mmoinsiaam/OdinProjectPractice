a788ca2d6072ba1804c200fdcc2f0dfc62d85188
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2024-08-10 18:42:43 -0400

    JS output now shows up on page instead of console.

--- JavaScriptFoundations/FizzBuzz problem/fizzbuzz.js
+++ JavaScriptFoundations/FizzBuzz problem/fizzbuzz.js
@@ -1,21 +1,22 @@
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz upto: "));
 
-const list = [];
+let earr = [];
 
 for(let i = 1; i <= answer; i++) {
     if(i % 3 === 0 && i % 5 === 0){
         console.log("FizzBuzz");
-        list.push("FizzBuzz");
+        earr.push("FizzBuzz");
     }
     else if(i % 3 === 0){
         console.log("Fizz");
-        list.push("Fizz");
+        earr.push("Fizz");
     }else if(i % 5 === 0){
         console.log("Buzz");
-        list.push("Buzz");   
+        earr.push("Buzz");   
     }else{
         console.log(i);
-        list.push(i);
+        earr.push(i);
     }
 }
 
+document.getElementById('list').innerHTML = earr;
