55d23619239f3b437610418550171f91d2a10595
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2024-08-13 00:15:08 -0400

    Added addItem function which adds an item to the list when the relevant button is clicked

--- JavaScriptFoundations/Shopping List/javascript.js
+++ JavaScriptFoundations/Shopping List/javascript.js
@@ -1,3 +1,21 @@
 const list = document.querySelector("#list");
 const btn = document.querySelector("#btn");
 const input = document.querySelector("#item");
+
+btn.addEventListener("click", addItem);
+
+function addItem() {
+    let itemName = input.value;
+    input.value = '';
+    const listItem = document.createElement("li");
+    const span = document.createElement("span");
+    const deleteBtn = document.createElement("button");
+
+    listItem.appendChild(deleteBtn);
+    listItem.appendChild(span);
+
+    list.appendChild(listItem);
+
+    deleteBtn.addEventListener("click", deleteItem);
+    input.focus();
+}
