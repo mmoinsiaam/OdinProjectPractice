c7bb8f5e35d8d8655c8d2b119d56408e3940d3c4
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2024-08-16 22:19:47 -0400

    added operate function which checks operator and calls for relevant function

--- JavaScriptFoundations/Calculator/javascript.js
+++ JavaScriptFoundations/Calculator/javascript.js
@@ -13,3 +13,15 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return (num1 / num2).toFixed(3); //Reduces ret val to 3 decimal places.
 }
+
+function operate(operator,num1,num2){
+    if(operator === "+"){
+        return add(num1,num2);
+    }else if(operator === "-"){
+        return subtract(num1,num2);
+    }else if(operator === "*"){
+        return multiply(num1,num2);
+    }else if(operator === "/"){
+        return divide(num1,num2);
+    }
+}
