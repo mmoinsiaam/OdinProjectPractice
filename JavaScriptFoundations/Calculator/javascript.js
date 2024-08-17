6afd0c648bc8db6d38131606fb2c2be87e319a02
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2024-08-16 22:11:30 -0400

    added 4 functions for basic calculations

---
+++ JavaScriptFoundations/Calculator/javascript.js
@@ -0,0 +1,15 @@
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+function divide(num1,num2){
+    return (num1 / num2).toFixed(3); //Reduces ret val to 3 decimal places.
+}
