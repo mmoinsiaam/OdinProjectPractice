340b785a67c38335d49373c8e1e398a59080679b
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2024-08-17 00:32:43 -0400

    added function to populate display

--- JavaScriptFoundations/Calculator/javascript.js
+++ JavaScriptFoundations/Calculator/javascript.js
@@ -1,13 +1,17 @@
 let arr = [];
 
+let display = document.querySelector("#display");
+
 let buttons = document.querySelectorAll(".number,.operator");
 let clearBtn = document.querySelector("#clear");
+let equalsBtn = document.querySelector(".equals");
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click",populateDisplay);
 }
 
-clearBtn.addEventListener("click",clear);
+//clearBtn.addEventListener("click",clear);
+//equalsBtn.addEventListener("click",evaluate);
 
 function add(num1,num2){
     return num1 + num2;
@@ -26,6 +30,11 @@ function divide(num1,num2){
 }
 
 function operate(operator,num1,num2){
+    if(isNaN(num1) || isNaN(num2)){
+        alert("Invalid Input");
+        return;
+    }
+    
     if(operator === "+"){
         return add(num1,num2);
     }else if(operator === "-"){
@@ -35,5 +44,16 @@ function operate(operator,num1,num2){
     }else if(operator === "/"){
         return divide(num1,num2);
     }
+}
+
+function populateDisplay(e) { 
+    let val = e.target.textContent;
+    if(isNaN(val)){
+        display.textContent = display.textContent + " " + val + " ";
+    }else{
+        display.textContent = display.textContent + val;
+    }
+    arr = display.textContent.split(" ").filter((word) => word !== "");
+    console.log(arr);
 }
 
