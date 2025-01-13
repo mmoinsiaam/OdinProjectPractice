88c030d03483196fa40d280dd460022e9a80652c
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2025-01-12 21:57:47 -0500

    Added style and functionality

--- JavaScript Intermediate/Library Project/javascript.js
+++ JavaScript Intermediate/Library Project/javascript.js
@@ -14,28 +14,68 @@ function addBookToLibrary(title,author,pages,readstat) {
     console.log("hasd");
 }
 
-addBookToLibrary("Harry","JK",245,"read");
-addBookToLibrary("asd","tqer",211,"not read");
+for (let index = 0; index < 50; index++) {
+    addBookToLibrary("asd" + index,"tqer",index,"Not Read");
+}
+
+function remFromLibrary(name) {
+    for (let index = 0; index < myLibrary.length; index++) {
+        if (myLibrary[index].Title === name) {
+            delete myLibrary[index];
+        }
+    }
+}
 
 for (let index = 0; index < myLibrary.length; index++) {
     console.log(myLibrary);
     const tempBook = myLibrary[index];
     const temp = document.createElement("div");
     temp.classList.add("book");
-    const unordered = document.createElement("ul");
-    const Title = document.createElement("li");
+    const Title = document.createElement("p");
     Title.innerText = `Title: ${tempBook.title}`;
-    const Author = document.createElement("li");
+    Title.classList.add("title");
+    const Author = document.createElement("p");
     Author.innerText = `Author: ${tempBook.author}`;
-    const Pages = document.createElement("li");
+    const Pages = document.createElement("p");
     Pages.innerText = `Pages: ${tempBook.pages}`;
-    const readStat = document.createElement("li");
+    const readStat = document.createElement("p");
     readStat.innerText = `Read Status: ${tempBook.readstat}`;
-    unordered.appendChild(Title);
-    unordered.appendChild(Author);
-    unordered.appendChild(Pages);
-    unordered.appendChild(readStat);
-    temp.appendChild(unordered);
+    readStat.classList.add("stat");
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove");
+    removeButton.innerText = "Remove";
+    const readButton = document.createElement("button");
+    readButton.classList.add("statchange");
+    readButton.innerText = "Read";
+
+    temp.appendChild(Title);
+    temp.appendChild(Author);
+    temp.appendChild(Pages);
+    temp.appendChild(readStat);
+    temp.appendChild(removeButton);
+    temp.appendChild(readButton);
+
+    removeButton.addEventListener("click", function(e) {
+        const parent = removeButton.parentNode;
+        const sibling = parent.querySelector(".title");
+        const name = sibling.innerText;
+        const titleName = name.split(' ')[1];
+        remFromLibrary(titleName);
+        parent.remove();
+    });
+
+    readButton.addEventListener("click", function(e) {
+        const parent = removeButton.parentNode;
+        const sibling = parent.querySelector(".stat");
+        const stat = sibling.innerText;
+        if (stat === "Read Status: Read") {
+            sibling.innerText = "Read Status: Not Read";
+        }else {
+            sibling.innerText = "Read Status: Read";
+        }
+    });
+
     holder.appendChild(temp);
     console.log("hi");
 }
+
